fc431b48971a101f53e71bb5d0d0a4a6f36f955e
erin williams <erin.cwilliams0@gmail.com>
2025-06-03 17:33:39 -0500

    Add project files.

---
+++ MiniProjectGuestBook/GuestBook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MiniProjectGuestBook
+{
+    public class GuestBook
+    {
+        Dictionary<string, int> GuestBookDetails { get; set; }
+
+        // first method propmpts user for name and party size then stores them
+        public void RegisterParty()
+        {
+            Console.Write("Please enter your full name: ");
+            string name = Console.ReadLine();
+            name = Regex.Replace(name, "[0-9]", "");
+
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.Write("That did not work please enter your first and last name: ");
+                name = Console.ReadLine();
+                name = Regex.Replace(name, "[0-9]", "");
+            }
+
+            Console.Write("Great now please enter the size of your party, you must enter a size greater than zero");
+            string partySizeText = Console.ReadLine();
+            bool isValidSize = int.TryParse(partySizeText, out int partySize);
+
+            while (!isValidSize || partySize < 1)
+            {
+                Console.Write("That didn't work please enter a valid party size: ");
+                partySizeText = Console.ReadLine();
+            }
+
+            GuestBookDetails.Add(name, partySize);
+        }
+
+        // second method gets the total party size from the guest book
+
+        //third method prints the guest list of names and a total party count
+        public void Print()
+        {
+            foreach (var party in GuestBookDetails)
+            {
+                Console.WriteLine(party.Key);
+            }
+        }
+    }
+}
---
+++ MiniProjectGuestBook/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MiniProjectGuestBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuestBook guestBook = new GuestBook();
+            int actionCode = 0;
+
+            while (actionCode != 3)
+            {
+                Console.WriteLine("Please read the following actions closely");
+                Console.WriteLine("1. Register a party");
+                Console.WriteLine("2. Print a list of guest names and total party count");
+                Console.WriteLine("3. Exit the application");
+                Console.WriteLine("Please select an action number: ");
+                string actionCodeText = Console.ReadLine();
+                bool isValidCode = int.TryParse(actionCodeText, out actionCode);
+
+                if (!isValidCode) 
+                {
+                    Console.WriteLine("\n that was not a valid code please try again");
+                    return;
+                }
+
+
+
+
+                Console.WriteLine("\n");
+            }
+
+            guestBook.Print();
+
+            Console.WriteLine();
+        }
+    }
+}
